4f242487a8bff7ea7213fd77d2d1ea2c665c99d0
Jehanlos <me@jehanlos.com>
2023-12-03 01:34:54 -0500

    Converted to Godot 4.x

--- Camera.gd
+++ Camera.gd
@@ -1,9 +1,9 @@
-extends Camera
+extends Camera3D
 
 # member variables here, example:
 # var a=2
 # var b="textvar"
-export var flyspeed= 2.0
+@export var flyspeed= 2.0
 var view_sensitivity = 0.3
 var mousedifference = Vector3()
 var yaw = 0
@@ -23,14 +23,14 @@ func _exit_tree():
 
 func _input(event):
 	if(event is InputEventKey):
-		if(event.scancode == KEY_ESCAPE):
+		if(event.keycode == KEY_ESCAPE):
 			self.get_tree().quit()
 	
 	if(event is InputEventMouseMotion):
 		yaw = fmod(yaw  - event.relative.x * view_sensitivity , 360)
 		pitch = max(min(pitch - event.relative.y * view_sensitivity, 90), -90)
-		self.rotation.x = deg2rad(pitch);
-		self.rotation.y = deg2rad(yaw);
+		self.rotation.x = deg_to_rad(pitch);
+		self.rotation.y = deg_to_rad(yaw);
 
 func _process(delta):
 	#mouse movement
@@ -40,15 +40,15 @@ func _process(delta):
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if Input.is_action_just_pressed("ui_page_up"):
-		OS.window_fullscreen = !OS.window_fullscreen
+		get_window().mode = Window.MODE_EXCLUSIVE_FULLSCREEN if (!((get_window().mode == Window.MODE_EXCLUSIVE_FULLSCREEN) or (get_window().mode == Window.MODE_FULLSCREEN))) else Window.MODE_WINDOWED
 	if(Input.is_key_pressed(KEY_W)):
-		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(0,0,1) * flyspeed * delta)
+		self.set_position(self.get_position() - get_global_transform().basis*Vector3(0,0,1) * flyspeed * delta)
 	if(Input.is_key_pressed(KEY_S)):
-		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(0,0,1) * flyspeed * -delta)
+		self.set_position(self.get_position() - get_global_transform().basis*Vector3(0,0,1) * flyspeed * -delta)
 	if(Input.is_key_pressed(KEY_A)):
-		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(1,0,0) * flyspeed * delta)
+		self.set_position(self.get_position() - get_global_transform().basis*Vector3(1,0,0) * flyspeed * delta)
 	if(Input.is_key_pressed(KEY_D)):
-		self.set_translation(self.get_translation() - get_global_transform().basis*Vector3(1,0,0) * flyspeed * -delta)
+		self.set_position(self.get_position() - get_global_transform().basis*Vector3(1,0,0) * flyspeed * -delta)
 	if(Input.is_key_pressed(KEY_E)):
 		translate(Vector3.UP*flyspeed*delta)
 	if(Input.is_key_pressed(KEY_Q)):
